152836c6a2de26f3f901234eba551e7ba2cbf801
lividlor <lorlivid2219@gmail.com>
2021-09-01 23:53:43 +0700

    Handling Functions and Handling Events

--- react-with-typescript/src/App.tsx
+++ react-with-typescript/src/App.tsx
@@ -1,25 +1,41 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import List from "./components/list"
 
-interface Istate {
-  people:{
-    name: string
-    age: number
-    url: string
-    note?:string
+import AddToList from './components/addtolist';
+import List from './components/list';
+
+export interface IState {
+  people: {
+      name: string
+      age: number
+      img: string
+      note?: string
   }[]
 }
+
+
 function App() {
-  const [people, setpeople]=useState<Istate["people"]>([])
-  people.map(person=>{
-    person.name
-  })
+
+  const [people, setPeople] = useState<IState["people"]>([
+    {
+      name: "LeBron James",
+      age: 35,
+      img: "https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png",
+      note: "Allegeric to staying on the same team",
+    },
+    {
+      name: "Kobe Bryant",
+      age: 42,
+      img: "https://fullpresscoverage.com/wp-content/uploads/2020/01/101524695-457220551.jpg"
+    }
+  ])
+
   return (
     <div className="App">
-      <h1>People invited</h1>
+      <h1>People Invited to my Party</h1>
       <List people={people}/>
+      <AddToList setPeople={setPeople} people={people}/>
     </div>
   );
 }
