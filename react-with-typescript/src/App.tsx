68d33a75ac49eeb6d97c67aa33c668e2184ebf32
lividlor <lorlivid2219@gmail.com>
2021-09-01 20:44:18 +0700

    Handling State

--- react-with-typescript/src/App.tsx
+++ react-with-typescript/src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import list from "./components/list"
 
+interface Istate {
+  people:{
+    name: string
+    age: number
+    url: string
+    note?:string
+  }[]
+}
 function App() {
+  const [people, setpeople]=useState<Istate["people"]>([])
+  people.map(person=>{
+    person.name
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>People invited</h1>
+      
     </div>
   );
 }
